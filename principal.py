c501a46ca811e47ed228afcec60092f93d1bd1be
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-06-26 20:28:39 -0300

    gerando arquivo para teste

--- principal.py
+++ principal.py
@@ -1,9 +1,6 @@
-#print("go fuck your self")
-
-def somar(x,y):
+    def somar(x,y):
         return x+y
+    
+    def sub(x, y):
+        return x-y
 
-n1 = int(input("Digite o 1 número: "))
-n2 = int(input("Digite o 2 número: "))
-
-print(somar(n1,n2))
---
+++ teste.py
@@ -0,0 +1,6 @@
+import pytest
+from principal import somar
+from principal import sub
+
+
+sub(2,2)
