75cd8b01361a514caed0dad5c2c372b94bdb4289
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-06-26 20:41:08 -0300

    criando travis.yml

--- teste.py
+++ teste.py
@@ -6,4 +6,7 @@ from principal import sub
 def test_somar():
     """ docstring for test somar"""
     assert somar(2, 3)==5
-    
+
+def test_sub():
+    """ docstring for test subtrair """
+    assert sub(2,2)==0
